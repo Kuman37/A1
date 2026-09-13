a7047bdf10fee85239fd52d2f99c470fdf50618a
Kuman37 <kuandykov5122@gmail.com>
2026-09-13 20:29:29 +0500

    Director

---
+++ src/AnimeDirector.java
@@ -0,0 +1,18 @@
+public class AnimeDirector {
+
+    public void constructCommunityDrivenAction(AnimeBuilder builder) {
+        builder.setTitle("Crimson Blade X")
+                .setGenre("Shonen Action")
+                .setPrimaryColorPalette("Black and Red")
+                .setEpisodeDurationMinutes(24)
+                .setStudio("Premium Animation Community");
+    }
+
+    public void constructGrimdarkFantasy(AnimeBuilder builder) {
+        builder.setTitle("Shadows of the balls")
+                .setGenre("Dark Fantasy / Horror")
+                .setPrimaryColorPalette("Monochrome with Crimson Accents")
+                .setEpisodeDurationMinutes(45)
+                .setStudio("Elite Art Studio");
+    }
+}
